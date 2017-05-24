a881f802c44f62695163c85eb342fa55fc758bd5
Stig Brautaset <stig@brautaset.org>
2017-05-25 00:20:18 +0200

    Give *an* answer (not the best!) when asked to price many books

--- src/potter/core.clj
+++ src/potter/core.clj
@@ -1,16 +1,21 @@
 (ns potter.core
   (:require [clojure.math.combinatorics :as combo]))
 
-(def discount {2 0.95
-               3 0.9
-               4 0.8
-               5 0.75})
+(def discounts {2 5/100
+                3 10/100
+                4 20/100
+                5 25/100})
+
+(defn multiplier [n]
+  (- 1 (or (discounts n) 0)))
+
+(def book-price 8)
 
 (defn price-partition
   [books]
   (let [unique (count (distinct books))
-        mult (or (discount unique) 1)]
-    (* 8
+        mult (multiplier unique)]
+    (* book-price
        (+ (* unique mult)
           (* (- (count books) unique))))))
 
@@ -20,10 +25,30 @@
        (map price-partition)
        (reduce +)))
 
-(defn price
+(defn best-price
   [books]
   (->> books
        combo/partitions
        (map price-partition-seq)
        sort
        first))
+
+(defn fast-price
+  [books]
+  (loop [counts (->> books frequencies vals sort)
+         total 0]
+    (if (seq counts)
+      (let [c (first counts)
+            n (count counts)
+            p (* c (price-partition (take n (iterate inc 1))))]
+        (recur (->> counts
+                    (map #(- % c))
+                    (remove zero?))
+               (+ total p)))
+      total)))
+
+(defn price
+  [books]
+  (if (< (count books) 10)
+    (best-price books)
+    (fast-price books)))
