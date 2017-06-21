d86ecc52ba83e97993fde28a796e4f5fa81a2aea
Stig Brautaset <stig@brautaset.org>
2017-06-21 17:33:54 +0100

    SUCCESS! Fast (enough), Correct, and Robust to changes in discounts

--- src/potter/core.clj
+++ src/potter/core.clj
@@ -1,37 +1,102 @@
-(ns potter.core)
+(ns potter.core
+  (:require [clojure.math.combinatorics :as combo]))
 
 (def book-price 8)
 
-(def fast-price-lookup
-  "Pre-calculated prices (with discount) for a stack of up to 5 distinct books.'"
-  {1 book-price
-   2 (* 2 book-price 95/100)
-   3 (* 3 book-price 9/10)
-   4 (* 4 book-price 8/10)
-   5 (* 5 book-price 75/100)})
+(def discounts
+  {2 5/100
+   3 10/100
+   4 20/100
+   5 25/100})
+
+(defn price-partition
+  "Price a partition of books."
+  [n]
+  (let [discount (or (discounts n) 0)
+        multiplier (- 1 discount)]
+    (* book-price n multiplier)))
+
+(def max-discount-books
+  (->> discounts keys sort last))
+
+(defn partition-patterns
+  "Candidates of book partitions from a set number of books."
+  [n]
+  (->> (repeat n 1)
+       combo/partitions
+       (map #(map count %))
+       (remove #(> (first %) max-discount-books))))
+
+(defn price-partitions
+  "Price collection of book partitions"
+  [parts]
+  (->> parts
+       (map price-partition)
+       (reduce +)))
+
+(defn sorted-price-patterns
+  "Zip prices & associated patterns together,
+  and sort by price so that best pattern comes first."
+  [prices patterns]
+  (->> (map vector prices patterns)
+       (sort-by first)))
+
+(defn pick-books
+  "Pick books according to indices given; return
+  remaining stacks of books."
+  [books indices]
+  ;;  (println "pick-books:" books indices)
+  (if (empty? indices)
+    (remove zero? books)
+    (recur (update-in (vec books) [(first indices)] dec)
+           (rest indices))))
+
+(defn book-combinations
+  "All the different combinations of indices to pick N books from a
+  collection of indexed piles of books."
+  [books n]
+  ;;  (println "book-combinations:" books n)
+  (combo/combinations (range (count books)) n))
+
+(defn pattern-match?
+  "Can we match this pattern to the books we are pricing?"
+  [patterns books]
+  (loop [books books
+         combinations (book-combinations books (first patterns))
+         patterns (rest patterns)
+         stack []]
+    ;;    (println "pattern-match?:" books combinations patterns stack)
+    (if (empty? books)
+      true
+      (if (empty? combinations)
+        (if (empty? stack)
+          false
+          (let [prev (peek stack)
+                b (nth prev 0)
+                c (nth prev 1)
+                p (nth prev 2)]
+            ;;            (println "pattern-match? - backtracking: " stack)
+            (recur b (rest c) p (pop stack))))
+        (let [remaining (pick-books books (first combinations))]
+          (if (empty? remaining)
+            true
+            (do
+              ;;              (println "pattern-match? - recursing: " remaining)
+              (recur remaining
+                     (book-combinations remaining (first patterns))
+                     (rest patterns)
+                     (conj stack [books combinations patterns])))))))))
 
 (defn price
   [books]
-
-  ;; Separate the books into piles of individual books
-  (loop [book-piles (->> books frequencies vals sort)
-         total 0]
-
-    ;; Any more piles of books left?
-    (if (seq book-piles)
-
-      ;; Do we hit the special case where two four-book stacks are
-      ;; cheaper than two stacks of three and five unique books each?
-      (if (= '(1 1 2 2 2) book-piles)
-
-        ;; Return current total plus the cost of two stacks of four
-        ;; unique books.
-        (+ total (* 2 (fast-price-lookup 4)))
-
-        ;; Take one book from each remaining pile & add the cost of
-        ;; this stack of books to the running total.
-        (recur (->> book-piles (map dec) (remove zero?))
-               (+ total (fast-price-lookup (count book-piles)))))
-
-      ;; No more piles of books left; return total.
-      total)))
+  (if (empty? books)
+    0
+    (let [n (count books)
+          patterns (partition-patterns n)
+          prices (map price-partitions patterns)
+          price-patterns (sorted-price-patterns prices patterns)
+          piles-of-distinct-books (-> books frequencies vals)]
+      (loop [[[pric pattern] & rest] price-patterns]
+        (if (pattern-match? pattern piles-of-distinct-books)
+          pric
+          (recur rest))))))
